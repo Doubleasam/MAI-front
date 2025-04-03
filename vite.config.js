5b1b65eaded7986d40c8d1cb7c413bb6417914d8
HeviSurafel <surafelwondu5647@gmail.com>
2025-04-03 17:11:35 +0400

    Fix PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       strategies: 'injectManifest',
       srcDir: 'src',
